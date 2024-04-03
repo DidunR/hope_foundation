d46013bafcdaa7d31444cd12c11fcfd3c469ede4
Roman <kolomiec58@gmail.com>
2024-04-03 17:56:09 +0300

    Hope groups corousel v02

--- hope_groups.js
+++ hope_groups.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
         speed: 300,
         slidesToShow:  1,
         adaptiveHeight: true,
-        slidesToScroll: 1, // Додай цей параметр
+        slidesToScroll: 1,
+        arrows: false,
         prevArrow: '<button type="button" class="slick-prev">Previous</button>',
         nextArrow: '<button type="button" class="slick-next">Next</button>'
     });
