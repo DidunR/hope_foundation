a0988db1a2d0334696a332e0168e53e7fe44dff9
kashalotik19@gmail.com <kashalotik19@gmail.com>
2024-04-05 22:14:02 +0300

    edited button read more

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
-// Отримання посилань на всі кнопки та контейнери тексту
-var toggleButtons = document.querySelectorAll(".toggleButton");
-var textContainers = document.querySelectorAll(".textContainer");
+let toggleButtons = document.querySelectorAll(".toggleButton");
+let textContainers = document.querySelectorAll(".textContainer");
 
-// Додавання обробника події для кожної кнопки
 toggleButtons.forEach(function (button, index) {
     button.addEventListener("click", function () {
-        // Перевірка поточного стану видимості тексту
-        if (textContainers[index].style.display === "none") {
-            // Якщо текст прихований, показати його
-            textContainers[index].style.display = "block";
+        var textContainer = textContainers[index];
+        if (textContainer.style.display === "none") {
+            // Плавне з'явлення тексту
+            textContainer.style.display = "block";
+            textContainer.style.opacity = "0";
+            setTimeout(function () {
+                textContainer.style.opacity = "1";
+            }, 100);
             button.textContent = "Hide";
+
+            // Прокручування до початку блока
+            var containerOffsetTop = textContainer.closest('.container').offsetTop;
+            window.scrollTo({
+                top: containerOffsetTop,
+                behavior: "smooth"
+            });
         } else {
-            // Якщо текст видимий, сховати його
-            textContainers[index].style.display = "none";
+            // Плавне зникнення тексту
+            textContainer.style.opacity = "0";
+            setTimeout(function () {
+                textContainer.style.display = "none";
+            }, 0);
             button.textContent = "Read more";
         }
     });
