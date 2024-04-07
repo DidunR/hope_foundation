f54df5b482f464aa65064d7e91bf271b965e875a
Roman <kolomiec58@gmail.com>
2024-04-07 18:15:46 +0300

    fixed read more for iphone

--- main.js
+++ main.js
@@ -14,10 +14,9 @@ toggleButtons.forEach(function (button, index) {
             button.textContent = "Hide";
 
             // Прокручування до початку блока
-            var containerOffsetTop = textContainer.closest('.container').offsetTop;
-            window.scrollTo({
-                top: containerOffsetTop,
-                behavior: "smooth"
+            textContainer.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
             });
         } else {
             // Плавне зникнення тексту
@@ -29,3 +28,38 @@ toggleButtons.forEach(function (button, index) {
         }
     });
 });
+
+
+
+
+// let toggleButtons = document.querySelectorAll(".toggleButton");
+// let textContainers = document.querySelectorAll(".textContainer");
+
+// toggleButtons.forEach(function (button, index) {
+//     button.addEventListener("click", function () {
+//         var textContainer = textContainers[index];
+//         if (textContainer.style.display === "none") {
+//             // Плавне з'явлення тексту
+//             textContainer.style.display = "block";
+//             textContainer.style.opacity = "0";
+//             setTimeout(function () {
+//                 textContainer.style.opacity = "1";
+//             }, 100);
+//             button.textContent = "Hide";
+
+//             // Прокручування до початку блока
+//             var containerOffsetTop = textContainer.closest('.container').offsetTop;
+//             window.scrollTo({
+//                 top: containerOffsetTop,
+//                 behavior: "smooth"
+//             });
+//         } else {
+//             // Плавне зникнення тексту
+//             textContainer.style.opacity = "0";
+//             setTimeout(function () {
+//                 textContainer.style.display = "none";
+//             }, 0);
+//             button.textContent = "Read more";
+//         }
+//     });
+// });
